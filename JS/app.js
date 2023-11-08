5ec4f60a77f21a42fca2ba925de959cc275713c1
Lara Ramsey <lararams3y@gmail.com>
2023-11-08 01:11:08 -0800

    added initial objects

---
+++ JS/app.js
@@ -0,0 +1,41 @@
+// put Global Variable here
+
+// put Helper Functions and Utilities here
+
+// put Object Literals here
+let seattle = {
+  name: 'Seattle',
+  minCust: 23,
+  maxCust: 65,
+  avgCookieBought: 6.3,
+}
+
+let tokyo = {
+  name: 'Tokyo'
+  minCust: 3,
+  maxCust: 24,
+  avgCookieBought: 1.2,
+}
+
+let dubai = {
+  name: 'Dubai'
+  minCust: 11,
+  maxCust: 38,
+  avgCookieBought: 3.7,
+}
+
+let paris = {
+  name: 'Paris'
+  minCust: 20,
+  maxCust: 38,
+  avgCookieBought: 2.3,
+}
+
+let lima = {
+  name: 'Lima'
+  minCust: 2,
+  maxCust: 16,
+  avgCookieBought: 4.6,
+}
+
+// put Executable Code here
