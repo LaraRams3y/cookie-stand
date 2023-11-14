a7e6809411aece0a11fcebabac9b7b2afd3f69a6
Lara Ramsey <lararams3y@gmail.com>
2023-11-13 21:38:47 -0800

    Seattle section after help from TA

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,6 @@
 // **PUT GLOBAL VARIABLES HERE**
-
-//let cookieStoreLocation = document.getElementById('cookie-store-locale');
-
-//console.dir(cookieStoreLocation);
+let salmonCookieSection = document.getElementById ('salmon-stuff');
+console.dir(salmonCookieSection);
 
 let hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm',];
 
@@ -14,7 +12,7 @@ let seattle = {
   minCust: 23,
   maxCust: 65,
   avgCookieBought: 6.3,
-  hourlyCustomers: 0,
+  hourlyCustomers: [],
   hourlyCookiesSold: [],
   totalCookies: 0,
     // **random cookie generator**
@@ -22,60 +20,65 @@ let seattle = {
     return Math.floor(Math.random() * (max - min + 1) + min); // inclusive min & max - got from MDN
   },
   getCustomers: function() {
-    this.hourlyCustomers = this.generateRandomCustomers(this.minCust,this.maxCust);
+    for (let i = 0; i < hours.length; i++){
+    this.hourlyCustomers.push(this.generateRandomCustomers(this.minCust,this.maxCust));
+    console.log(this.hourlyCustomers);
+    }
   },
   
   //loop that goes through hours array and creates number of customers and multiplies cookies
   render: function (){
+    this.getCustomers();
     for (let i = 0; i < hours.length; i++){
-      let cookies = (this.avgCookieBought * this.getCustomers());
-      console.log(cookies)
+      console.log(this.hourlyCustomers);
+      let cookies = (this.avgCookieBought * this.hourlyCustomers[i]);
+      console.log(cookies);
       
-      this.totalCookies.toFixed(2) += cookies
-      console.log (this.totalCookies)
+      this.totalCookies += cookies;
+      console.log (this.totalCookies);
 
-      this.hourlyCookiesSold.push(cookies.toFixed(2))
+      this.hourlyCookiesSold.push(cookies.toFixed(2));
     }
-    console.log(this.hourlyCookiesSold)
+    console.log(this.hourlyCookiesSold);
   }
-}
+};
   
 
-let tokyo = {
-  name: 'Tokyo'
-  minCust: 3,
-  maxCust: 24,
-  avgCookieBought: 1.2,
-}
+//let tokyo = {
+//  name: 'Tokyo'
+//   minCust: 3,
+//   maxCust: 24,
+//   avgCookieBought: 1.2,
+// }
 
-let dubai = {
-  name: 'Dubai'
-  minCust: 11,
-  maxCust: 38,
-  avgCookieBought: 3.7,
-}
+// let dubai = {
+//   name: 'Dubai'
+//   minCust: 11,
+//   maxCust: 38,
+//   avgCookieBought: 3.7,
+// }
 
-let paris = {
-  name: 'Paris'
-  minCust: 20,
-  maxCust: 38,
-  avgCookieBought: 2.3,
-}
+// let paris = {
+//   name: 'Paris'
+//   minCust: 20,
+//   maxCust: 38,
+//   avgCookieBought: 2.3,
+// }
 
-let lima = {
-  name: 'Lima'
-  minCust: 2,
-  maxCust: 16,
-  avgCookieBought: 4.6,
-}
+// let lima = {
+//   name: 'Lima'
+//   minCust: 2,
+//   maxCust: 16,
+//   avgCookieBought: 4.6,
+// }
 
-// **PUT EXECUTABLE CODE HERE**
+// // **PUT EXECUTABLE CODE HERE**
 
-console.log(seattle);
+// console.log(seattle);
 
 seattle.render();
-tokyo.render();
-dubai.render();
-paris.render();
-lima.render();
+// tokyo.render();
+// dubai.render();
+// paris.render();
+// lima.render();
 
