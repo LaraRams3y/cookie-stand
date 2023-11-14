d6be2f7b9ba4e8e0c01355e0af7c83db295fb4c8
Lara Ramsey <lararams3y@gmail.com>
2023-11-13 17:22:52 -0800

    more fixs to Seattle object

--- JS/app.js
+++ JS/app.js
@@ -17,50 +17,58 @@ let seattle = {
   hourlyCustomers: 0,
   hourlyCookiesSold: [],
   totalCookies: 0,
+
   // **random cookie generator**
-  randomCustomersGenerator: function(min,max){
+  generateRandomCustomers: function(min,max){
     return Math.floor(Math.random() * (max - min + 1) + min) // inclusive min & max - got from MDN
   },
   getCustomers: function() {
-    this.hourlyCustomers = this.randomCustomersGenerator(this.minCust,this.maxCust);
+    this.hourlyCustomers = this.generateRandomCustomers(this.minCust,this.maxCust);
     return this.hourlyCustomers;
   },
-  render: function(){
-    this.getRandomCustomers();
-  },
+  
   //loop that goes through hours array and creates number of customers and multiplies cookies
   render: function (){
-    
+    for (let i = 0; i < hours.length; i++){
+      let cookies = (this.avgCookieBought * this.getCustomers());
+      console.log(cookies)
+      
+      this.totalCookies.toFixed(2) += cookies
+      console.log (this.totalCookies)
+
+      this.hourlyCookiesSold.push(cookies.toFixed(2))
+    }
+    console.log(this.hourlyCookiesSold)
   }
-};
+}
 
 let tokyo = {
   name: 'Tokyo'
   minCust: 3,
   maxCust: 24,
   avgCookieBought: 1.2,
-};
+}
 
 let dubai = {
   name: 'Dubai'
   minCust: 11,
   maxCust: 38,
   avgCookieBought: 3.7,
-};
+}
 
 let paris = {
   name: 'Paris'
   minCust: 20,
   maxCust: 38,
   avgCookieBought: 2.3,
-};
+}
 
 let lima = {
   name: 'Lima'
   minCust: 2,
   maxCust: 16,
   avgCookieBought: 4.6,
-};
+}
 
 // put Executable Code here
 
