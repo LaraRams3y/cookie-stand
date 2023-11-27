b2e255a3438d425212c66942f12b9d321dc5177a
Lara Ramsey <lararams3y@gmail.com>
2023-11-26 21:04:17 -0800

    created all stores details info object

---
+++ JS/index.js
@@ -0,0 +1,55 @@
+
+
+//***NEW FUNCTION
+
+// **GLOBAL VARIABLES HERE**
+const allStoreDeetsArray = [];
+
+//***GRAB THE WINDOW INTO THE DOM**
+const ourStoresSection = document.getElementById('our-stores');
+console.dir(ourStoresSection);
+
+//**HELPER FUNCTIONS & UTILITIES HERE**
+function renderAllStoreDeets(){
+  for (let i = 0; i < allStoreDeetsArray.length; i++){
+    allStoreDeetsArray[i].render();
+  }
+}
+
+//***CONSTRUCTOR FUNCTIONS***
+function StoreDeets(cityName, address, operatingHours, contact){
+  this.cityName = cityName;
+  this.address = address;
+  this.operatingHours = operatingHours;
+  this.contact = contact;
+  allStoreDeetsArray.push(this);
+}
+
+//Prototypes & Methods
+StoreDeets.prototype.render = function () {
+  let articleEle = document.createElement('article');
+  ourStoresSection.appendChild(articleEle);
+
+  let storeName = document.createElement('h2');
+  storeName.innerText = this.cityName;
+  articleEle.appendChild(storeName);
+
+  let storeInfoUL = document.createElement('ul');
+  articleEle.appendChild(storeInfoUL);
+
+  for (let i = 0; i < allStoreDeetsArray.length; i++){
+    let allStoreDeetsArrayLI = document.createElement('li');
+    allStoreDeetsArrayLI.innerText = allStoreDeetsArray[i][1];
+    storeInfoUL.appendChild(allStoreDeetsArrayLI);
+  }
+};
+
+new StoreDeets('Seattle', '600 Broadway', '6am to 7pm', 'Seattle@SalmonCookies.bake');
+new StoreDeets('Tokyo', '3 Omotesando', '6am to 7pm', 'Tokyo@SalmonCookies.bake');
+new StoreDeets('Dubai', '11 Al Faheidi Street', '6am to 7pm', 'Dubai@SalmonCookies.bake');
+new StoreDeets('Paris', '20 Champs-Élysées', '6am to 7pm', 'Paris@SalmonCookies.bake');
+new StoreDeets('Lima', 'Av. Garcilaso de la Vega 1337', '6am to 7pm', 'Lima@SalmonCookies.bake');
+
+renderAllStoreDeets();
+
+
